fd77130b7269fb2b1bcef8cafda8ba82ae694247
oliver.ferrigni <oliver.ferrigni@asolutions.com>
2012-06-26 06:25:30 -0500

    Initial commit of separating automocking container to another project.
    Oliver

---
+++ test/com/oliver/workout/trainer/test/support/MockingPicoContainer.java
@@ -0,0 +1,101 @@
+package com.oliver.workout.trainer.test.support;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.behaviors.Caching;
+import org.picocontainer.containers.AbstractDelegatingMutablePicoContainer;
+
+public class MockingPicoContainer<M> extends
+		AbstractDelegatingMutablePicoContainer {
+
+	public final Mockery context;
+	public M sut;
+
+	public MockingPicoContainer() {
+		this(new DefaultPicoContainer(new Caching()), new Mockery());
+	}
+
+	public MockingPicoContainer(Class<M> sutClazz)  {
+		this(new DefaultPicoContainer(new Caching()), new Mockery(), sutClazz);
+	}
+
+	MockingPicoContainer(MutablePicoContainer delegate, Mockery context) {
+		super(delegate);
+		context.setImposteriser(ClassImposteriser.INSTANCE);
+		this.context = context;
+	}
+
+	MockingPicoContainer(DefaultPicoContainer delegate, Mockery context,
+			Class<M> sutClazz){
+		super(delegate);
+		context.setImposteriser(ClassImposteriser.INSTANCE);
+		this.context = context;
+		try {
+			generateSut(sutClazz);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public M generateSut(Class<M> clazz) throws Exception {
+		Constructor<M>[] constructors = (Constructor<M>[]) clazz
+				.getConstructors();
+		Constructor<M> widest = null;
+		int paramCount = 0;
+		for (Constructor<M> constructor : constructors) {
+			if (paramCount < constructor.getParameterTypes().length) {
+				paramCount = constructor.getParameterTypes().length;
+				widest = constructor;
+			}
+		}
+		if (widest != null) {
+			Class<?>[] parameterTypes = widest.getParameterTypes();
+			List<Object> args = new ArrayList<Object>();
+			for (Class<?> class1 : parameterTypes) {
+				if (this.getComponents(class1).isEmpty()) {
+					Object mock = context.mock(class1);
+					this.addComponent(class1, mock);
+					args.add(mock);
+				}
+			}
+
+			sut = widest.newInstance((Object[]) args.toArray(new Object[args
+					.size()]));
+		} else {
+			sut = clazz.newInstance();
+		}
+		return sut;
+	}
+
+	public <T> T get(Class<T> componentType) {
+		return getComponent(componentType);
+	}
+
+	@Override
+	public <T> T getComponent(Class<T> componentType) {
+		if (this.getComponents(componentType).isEmpty()) {
+			Object mock = context.mock(componentType);
+			this.addComponent(componentType, mock);
+		}
+		return super.getComponent(componentType);
+	}
+
+	public void checking(Expectations expectations) {
+		context.checking(expectations);
+	}
+
+	public void verify() {
+		context.assertIsSatisfied();
+	}
+}
---
+++ test/com/oliver/workout/trainer/test/support/MockingPicoContainerTest.java
@@ -0,0 +1,104 @@
+package com.oliver.workout.trainer.test.support;
+
+import static org.junit.Assert.*;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Before;
+import org.junit.Test;
+import org.picocontainer.DefaultPicoContainer;
+import org.picocontainer.behaviors.Caching;
+
+public class MockingPicoContainerTest {
+	private DefaultPicoContainer delegate;
+	private Mockery context;
+	@Before
+	public void create_mocks(){
+		delegate = new DefaultPicoContainer(new Caching());
+		context = new Mockery();
+		context.setImposteriser(ClassImposteriser.INSTANCE);
+	}
+
+	@Test
+	public void exploration() throws Exception {
+		MockingPicoContainer<ToBeMocked1> autoMocked = new MockingPicoContainer<ToBeMocked1>(delegate, context, ToBeMocked1.class);
+		assertSame(delegate.getComponent(Mock1.class),autoMocked.sut.arg1);
+	}
+	
+	@Test
+	public void exploration2() throws Exception {
+		MockingPicoContainer<ToBeMocked2> autoMocked = new MockingPicoContainer<ToBeMocked2>(delegate, context, ToBeMocked2.class);
+		assertSame(delegate.getComponent(Mock1.class),autoMocked.sut.arg1);
+		assertSame(delegate.getComponent(Mock2.class),autoMocked.sut.arg2);
+	}	
+	@Test
+	public void exploration3() throws Exception {
+		MockingPicoContainer<ToBeMocked3> autoMocked = new MockingPicoContainer<ToBeMocked3>(delegate, context, ToBeMocked3.class);
+		assertSame(delegate.getComponent(Mock1.class),autoMocked.sut.arg1);
+		assertSame(delegate.getComponent(Mock2.class),autoMocked.sut.arg2);
+		assertSame(delegate.getComponent(Mock3.class),autoMocked.sut.arg3);
+	}
+	@Test
+	public void verify() throws Exception {
+		final MockingPicoContainer<Foo1> autoMocked = new MockingPicoContainer<Foo1>(delegate, context, Foo1.class);
+		assertSame(delegate.getComponent(Mock1.class),autoMocked.sut.arg1);
+		autoMocked.checking(new Expectations(){{ one(autoMocked.get(Mock1.class)).baz();}});
+		autoMocked.sut.bar();
+		autoMocked.verify();
+	}
+
+	public static class Mock1 {
+		public void baz() {
+		}
+	}
+	public static class Mock2 {
+	}
+	public static class Mock3 {
+	}
+	
+	public static class ToBeMocked1 {
+		public final Mock1 arg1;
+
+		public ToBeMocked1(Mock1 arg1){
+			this.arg1 = arg1;
+		}
+	}
+	public static class ToBeMocked2 {
+		public  Mock1 arg1;
+		public  Mock2 arg2;
+		public ToBeMocked2(Mock1 arg1){
+			this.arg1 = arg1;
+		}
+		public ToBeMocked2(Mock1 arg1, Mock2 arg2){
+			this.arg1 = arg1;
+			this.arg2 = arg2;
+		}
+	}
+	public static class ToBeMocked3 {
+		public  Mock1 arg1;
+		public  Mock2 arg2;
+		public  Mock3 arg3;
+		public ToBeMocked3(Mock1 arg1){
+		}
+		public ToBeMocked3(Mock1 arg1, Mock2 arg2){
+		}
+		public ToBeMocked3(Mock1 arg1, Mock2 arg2, Mock3 arg3){
+			this.arg1 = arg1;
+			this.arg2 = arg2;
+			this.arg3 = arg3;
+		}
+	}
+	
+	public static class Foo1 {
+		private final Mock1 arg1;
+
+		public Foo1(Mock1 arg1) {
+			this.arg1 = arg1;
+		}
+		
+		public void bar() {
+			arg1.baz();
+		}
+	}
+}
