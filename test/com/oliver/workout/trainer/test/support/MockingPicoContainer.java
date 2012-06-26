ebedb1a38079f617906767b3b876579e5b71c3d2
oliver.ferrigni <oliver.ferrigni@asolutions.com>
2012-06-26 06:35:50 -0500

    Organized imports.
    Oliver

--- test/com/oliver/workout/trainer/test/support/MockingPicoContainer.java
+++ test/com/oliver/workout/trainer/test/support/MockingPicoContainer.java
@@ -1,7 +1,6 @@
 package com.oliver.workout.trainer.test.support;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
